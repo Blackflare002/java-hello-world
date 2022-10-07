e9755d330c15a4cede706d9f562bc4035673d77d
Shane Phillips <shane@loongphillips.com>
2022-10-07 15:40:28 -0400

    first commit

---
+++ tests.java
@@ -0,0 +1,11 @@
+public class tests {
+    public static void main(String[] args) {
+        // String name = "Astolfo";
+        // System.out.println(name.toUpperCase());
+        addEx("weiners");
+    }
+
+    public static void addEx(String s) {
+        System.out.println(s + "!");
+    }
+}
