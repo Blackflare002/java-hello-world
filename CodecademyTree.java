b5803d9d6ba5a144e489e39b50cfd8bcd9bc2cfa
Shane Phillips <shane@loongphillips.com>
2022-10-11 15:27:28 -0400

    add two small projects, show compiled code.

---
+++ CodecademyTree.java
@@ -0,0 +1,13 @@
+public class CodecademyTree {
+    public static void main(String[] args) {
+        // This will introduce you and print a tree
+        System.out.println("Hello, I'm Shane, and I'm learning Java!");
+        System.out.println("And now, here's a tree:");
+        System.out.println("* ** *");
+        System.out.println("******");
+        System.out.println(" ****");
+        System.out.println("  **");
+        System.out.println("  **");
+        System.out.println("~~~~~~~~~~");
+    }
+}
